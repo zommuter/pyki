5b5cb4bda568e74f2427a9c170daf8cd14774b29
Tobias Kienzler <tobias.kienzler@gmx.de>
2015-11-28 23:08:09 +0100

    Linkified dir ls, renamed 'path' to 'relpath' to decrease confusion (I hope)

--- pyki/__init__.py
+++ pyki/__init__.py
@@ -8,16 +8,22 @@ app = Flask(__name__)
 #here = os.path.abspath(os.path.dirname(__file__))
 here = os.getcwd()
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def parse_path(path):
-    fullpath = os.path.join(here, path)
+def linkify(parent, name):
+    relpath = os.path.join(parent, name)
+    return '<a href="{relpath}">{name}</a>'.format(relpath=relpath, name=name)
+
+@app.route('/', defaults={'relpath': ''})
+@app.route('/<path:relpath>')
+def parse_path(relpath):
+    fullpath = os.path.join(here, relpath)
+    msg = fullpath + "<p>"
     if os.path.isdir(fullpath):
-        return '<br>'.join(os.listdir(fullpath))
+        msg +='<br>'.join([linkify(relpath, f) for f in os.listdir(fullpath)])
     elif os.path.isfile(fullpath):
-        return fullpath + " is a file!"
+        msg += fullpath + " is a file!"
     else:
-        return fullpath + " not found!"
+        msg += fullpath + " not found!"
+    return msg
 
 if __name__ == '__main__':
     app.run()
